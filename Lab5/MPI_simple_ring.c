d2860fd4e2c27f58ff3307fb0ac0d4cbf25d8b0f
DaoistDuck <dragonoath123@gmail.com>
2021-03-05 12:51:36 -0800

    two working programs
    working correctly atm

---
+++ Lab5/MPI_simple_ring.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <mpi.h>
+
+/*
+Using the above MPI_simple.c as a model, write a program that sends a message around a ring of
+processes. 
+More precisely, processes i>0 should wait to receive a message from the process with rank i-1, 
+verify that the message contains the integer i-1, 
+replace the integer i-1 with the integer i, and then 
+send the modified message to the process with rank i+1. 
+
+For example, 
+the process with rank 0 should send a message containing its rank to the process with rank 1, 
+receive a message from the process with rank n-1, and then 
+print the message to the screen.
+
+Test the program with different arguments for –np witch, so that you are confident it works correctly.
+Measure the execution time with different number of MPI processes.
+
+*/
+
+int main(int argc, char *argv[]) {
+
+    int numprocs, rank, namelen,nextproc,prevproc,i,token;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    MPI_Status status;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    nextproc = (rank + 1) % numprocs;
+    prevproc = (rank + numprocs - 1)% numprocs;
+    i = 0;
+
+    
+    if(rank == 0){
+        MPI_Send(&i, 1, MPI_INT,(rank + 1) % numprocs,0,MPI_COMM_WORLD);
+        i++;
+    }
+
+    if(rank > 0){
+        MPI_Recv(&i, numprocs, MPI_INT, (rank + numprocs - 1)% numprocs,0,MPI_COMM_WORLD,&status);
+        prevproc = (rank + numprocs - 1)% numprocs;
+        printf("Process %d received i value %d from Process %d\n", rank, i, prevproc);
+        i++;
+        MPI_Send(&i, 1, MPI_INT,(rank + 1) % numprocs,0,MPI_COMM_WORLD);
+    }
+
+    if(rank == 0){
+        MPI_Recv(&i, numprocs, MPI_INT, prevproc,0,MPI_COMM_WORLD,&status);
+        printf("Process %d received i value %d from Process %d\n", rank, i, prevproc);
+    }
+
+
+    MPI_Finalize();
+
+    return 0;
+}
